a37cb8e30f8f511293e6192a246a9773380e1f6d
1submarine <submarine31@yandex.com>
2021-07-29 13:42:35 -0400

    Added AC_CHECK_HEADER_STDBOOL

--- config.h
+++ config.h
@@ -1,25 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Name of package */
-#undef PACKAGE
+#define PACKAGE "autotools-template"
 
 /* Define to the address where bug reports for this package should be sent. */
-#undef PACKAGE_BUGREPORT
+#define PACKAGE_BUGREPORT ""
 
 /* Define to the full name of this package. */
-#undef PACKAGE_NAME
+#define PACKAGE_NAME "Autotools Template"
 
 /* Define to the full name and version of this package. */
-#undef PACKAGE_STRING
+#define PACKAGE_STRING "Autotools Template v0.0"
 
 /* Define to the one symbol short name of this package. */
-#undef PACKAGE_TARNAME
+#define PACKAGE_TARNAME "autotools-template"
 
 /* Define to the home page for this package. */
-#undef PACKAGE_URL
+#define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#undef PACKAGE_VERSION
+#define PACKAGE_VERSION "v0.0"
 
 /* Version number of package */
-#undef VERSION
+#define VERSION "v0.0"
